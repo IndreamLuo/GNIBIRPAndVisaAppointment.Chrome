913793b81c54f19548891054b984aeedc7c4d24f
Indream <indreamluo@qq.com>
2018-03-06 23:39:01 +0000

    Add Loading and Empty inform
    Add loading and empty inform in the beginning and end.

--- Content/portal.js
+++ Content/portal.js
@@ -1,7 +1,9 @@
 var decoders = {
     visa: function (type, data) {
+        var $list = decoders.$lists['visa'] || (decoders.$lists['visa'] = $('.visas .list'));
+
         if (data.dates && data.dates.length && data.dates[0] != "01/01/1900") {
-            var $types = decoders.$getTypeGroup($('.visas .list'), type);
+            var $types = decoders.$getTypeGroup($list, type);
             for (var index in data.dates) {
                 var appointment = data.dates[index];
                 $types
@@ -10,8 +12,10 @@ var decoders = {
         }
     },
     irp: function (type, data) {
+        var $list = decoders.$lists['irp'] || (decoders.$lists['irp'] = $('.irps .list'));
+
         if (data.slots && data.slots.length) {
-            var $types = decoders.$getTypeGroup($('.irps .list'), type);
+            var $types = decoders.$getTypeGroup($list, type);
             for (var index in data.slots) {
                 var appointment = data.slots[index];
                 $types
@@ -19,6 +23,7 @@ var decoders = {
             }
         }
     },
+    $lists: {},
     $getTypeGroup: function ($list, type) {
         if (!$list.find('[type=' + type + ']').length) {
             !$list.append('<div class="typegroup" type="' + type + '"><div class="type">' + type + '</div></div>');
@@ -28,43 +33,76 @@ var decoders = {
     }
 };
 
+var statusControl = {
+    loadings: {},
+    addLoading: function (group) {
+        statusControl.loadings[group] = (statusControl.loadings[group] || 0) + 1;
+    },
+    removeLoading: function (group) {
+        var listSelector = '.' + group + 's .list';
+        $(listSelector + ' .loading').remove();
+        statusControl.loadings[group]--;
+
+        if (!statusControl.loadings[group] && !$(listSelector + ' .appointment').length) {
+            $(listSelector).append('<div class="empty">There\'s no valid appointment.');
+        }
+    }
+};
+
+var groups = {
+    visa: 'visa',
+    irp: 'irp'
+};
+
 var targets = [{
     type: 'Individual',
     url: 'https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getDTAvail)?openagent&type=I',
-    decode: decoders.visa
+    group: groups.visa
 }, {
     type: 'Family',
     url: 'https://reentryvisa.inis.gov.ie/website/INISOA/IOA.nsf/(getDTAvail)?openagent&type=F',
-    decode: decoders.visa
+    group: groups.visa
 }, {
     type: "Work-New",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=New",
-    decode: decoders.irp
+    group: groups.irp
 }, {
     type: "Work-Renewal",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=Renewal",
-    decode: decoders.irp
+    group: groups.irp
 }, {
     type: "Study-New",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Study&sbcat=All&typ=New",
-    decode: decoders.irp
+    group: groups.irp
 }, {
     type: "Study-Renewal",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Study&sbcat=All&typ=Renewal",
-    decode: decoders.irp
+    group: groups.irp
 }, {
     type: "Other-New",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Other&sbcat=All&typ=New",
-    decode: decoders.irp
+    group: groups.irp
 }, {
     type: "Other-Renewal",
     url: "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Other&sbcat=All&typ=Renewal",
-    decode: decoders.irp
+    group: groups.irp
 }];
 
 $(document).ready(function () {
+    $('.group .appointlink').click(function () {
+        var newURL = $(this).attr('href');
+        var newTab = chrome.tabs.create({
+            url: newURL,
+            //active: false
+        }, function (tab) {;
+            
+        });
+        return false;
+    });
+
     for (var index in targets) {
         var target = targets[index];
+        statusControl.addLoading(target.group);
         (function (target) {
             $.ajax({
                 url: target.url,
@@ -73,7 +111,8 @@ $(document).ready(function () {
                     var error = jqXHR.errorMessage;
                 },
                 success: function (data, jqXHR, textStatus) {
-                    target.decode(target.type, data);
+                    decoders[target.group](target.type, data);
+                    statusControl.removeLoading(target.group);
                 }
             });
         })(target);
