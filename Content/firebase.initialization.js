7ae9a403b5d8f0f0e582e776ba3a64ac9a25b265
Indream Luo <indreamluo@qq.com>
2018-09-08 21:53:04 +0000

    Move config out from firebase.initialization

--- Content/firebase.initialization.js
+++ Content/firebase.initialization.js
@@ -1,10 +1,2 @@
 // Initialize Firebase
-var config = {
-apiKey: "AIzaSyC96pCo4_h2TqEurKwBS7w8OhQ9DeuWG_M",
-authDomain: "visa-and-irp-gnib-appointment.firebaseapp.com",
-databaseURL: "https://visa-and-irp-gnib-appointment.firebaseio.com",
-projectId: "visa-and-irp-gnib-appointment",
-storageBucket: "visa-and-irp-gnib-appointment.appspot.com",
-messagingSenderId: "164032443416"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(config.firebase);
